be229a425325cb7d9366e4c0f3796917c9d07f64
paularinzee <arinzennajipual@gmail.com>
2023-05-30 17:43:40 +0100

    new_config

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_XuzccE6J4', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '3klaobk7d9eq5imr4peq8q2b6u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
