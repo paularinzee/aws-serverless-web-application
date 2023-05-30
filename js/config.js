cf7173a427ed10351caa5c3693a1cf634351aae8
paularinzee <arinzennajipual@gmail.com>
2023-05-30 17:34:45 +0100

    new_config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_XuzccE6J4', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3klaobk7d9eq5imr4peq8q2b6u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
